e2d1d01804561e2ac6c426df77fcb9c61f47faba
Jeffrey Bennet <67395829+JeffreyBennet@users.noreply.github.com>
2024-07-20 23:00:28 -0500

    Delete services directory

--- services/github_service.py
+++
@@ -1,9 +0,0 @@
-import os
-from git import Repo
-
-def clone_repository(repo_url):
-    repo_path = "/tmp/repo"
-    if os.path.exists(repo_path):
-        os.system(f'rm -rf {repo_path}')
-    Repo.clone_from(repo_url, repo_path)
-    return repo_path
--- services/s3_service.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-def upload_to_s3(repo_path, bucket_name='myBucket', s3_directory='repo'):
-    s3_client = boto3.client('s3')
-    for root, dirs, files in os.walk(repo_path):
-        for file in files:
-            s3_client.upload_file(os.path.join(root, file), bucket_name, os.path.join(s3_directory, file))
