f96274173f2b98faaa1342a2ca86eab568381c62
Jeffrey Bennet <67395829+JeffreyBennet@users.noreply.github.com>
2024-07-22 19:16:32 -0500

    Delete services directory

--- services/clone_service.py
+++
@@ -1,8 +0,0 @@
-import git
-
-def clone_repository(repo_url):
-    local_path = "/tmp/repo"
-    git.Repo.clone_from(repo_url, local_path)
-    return local_path
-logging.info("Repository cloned to local path: %s", local_path)
- # Insert at line: 7
